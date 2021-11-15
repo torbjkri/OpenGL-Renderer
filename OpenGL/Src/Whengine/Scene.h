c528c732358f552c17696807104ca9cc29084f12
torbjkri <torbjorn.kringeland88@gmail.com>
2021-11-15 23:27:34 +0100

    More setting up

--- OpenGL/Src/Whengine/Scene.h
+++ OpenGL/Src/Whengine/Scene.h
@@ -4,26 +4,25 @@
 #include <memory>
 
 #include <GL/Renderable/RenderableObject.h>
+#include <GL/Camera.h>
+#include <GL/Renderable/Light.h>
+#include <GL/Renderable/ObjectRenderer.h>
 
 namespace WE {
 
 class Scene {
 private:
-	std::vector<std::unique_ptr<RenderableObject>>
-	//-//-//-//-//-//-//-//-//
-	// Renderable Objects;  //
-	// Static solid Renderable     //
-	// Lights               //
-	// Camera               //
-	//-//-//-//-//-//-//-//-//
+	std::vector<std::unique_ptr<RenderableObject>> m_Objects;
+	Camera m_ViewCamera;
+	Light m_Light;
+	ObjectRenderer m_Renderer;
+
 public:
 
 	void Update();
 
-	// Is visitor correct?
-	void Render(const Renderer& renderer);
-
 	//Maybe not needed
+	void RenderScene();
 	void RenderGui();
 };
 }
