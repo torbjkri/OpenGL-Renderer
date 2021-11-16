8f6ac3478340938f421108540f263d5d3fb52e2b
torbjkri <torbjorn.kringeland88@gmail.com>
2021-11-16 19:18:02 +0100

    Finish inputHander

--- OpenGL/Src/GL/InputHandler.cpp
+++ OpenGL/Src/GL/InputHandler.cpp
@@ -71,9 +71,14 @@ void InputHandler::HandleMouse()
 		glfwSetInputMode(m_Window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
 
 		// Prevents camera from jumping on the first click
+		int width;
+		int height;
+
+		glfwGetWindowSize(m_Window, &width, &height);
+
 		if (firstClick)
 		{
-			glfwSetCursorPos(m_Window, (m_Width / 2), (m_Height / 2));
+			glfwSetCursorPos(m_Window, (width / 2), (height / 2));
 			firstClick = false;
 		}
 
@@ -83,74 +88,21 @@ void InputHandler::HandleMouse()
 		// Fetches the coordinates of the cursor
 		glfwGetCursorPos(m_Window, &mouseX, &mouseY);
 
-		
+		m_Camera->MouseTurn(mouseX, mouseY);
+
 
 		// Sets mouse cursor to the middle of the screen so that it doesn't end up roaming around
-		glfwSetCursorPos(window, (m_Width / 2), (m_Height / 2));
+		glfwSetCursorPos(m_Window, (width / 2), (height / 2));
 
 	}
-	else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE)
+	else if (glfwGetMouseButton(m_Window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE)
 	{
 		// ToDo: This runs every frame, should have a shielding
 		//	     Preferably mouse and KB IO should be handled by a separate class
 		// 
 		// Unhides cursor since camera is not looking around anymore
-		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+		glfwSetInputMode(m_Window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 		// Makes sure the next time the camera looks around it doesn't jump
 		firstClick = true;
 	}
-
-}void Camera::HandleMouse(GLFWwindow* window)
-{
-	// Handles mouse inputs
-	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
-	{
-		// Hides mouse cursor
-		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
-
-		// Prevents camera from jumping on the first click
-		if (firstClick)
-		{
-			glfwSetCursorPos(window, (m_Width / 2), (m_Height / 2));
-			firstClick = false;
-		}
-
-		// Stores the coordinates of the cursor
-		double mouseX;
-		double mouseY;
-		// Fetches the coordinates of the cursor
-		glfwGetCursorPos(window, &mouseX, &mouseY);
-
-		// Normalizes and shifts the coordinates of the cursor such that they begin in the middle of the screen
-		// and then "transforms" them into degrees 
-		float rotX = sensitivity * (float)(mouseY - (m_Height / 2)) / m_Height;
-		float rotY = sensitivity * (float)(mouseX - (m_Width / 2)) / m_Width;
-
-		// Calculates upcoming vertical change in the Orientation
-		glm::vec3 newOrientation = glm::rotate(Orientation, glm::radians(-rotX), glm::normalize(glm::cross(Orientation, Up)));
-
-		// Decides whether or not the next vertical Orientation is legal or not
-		if (abs(glm::angle(newOrientation, Up) - glm::radians(90.0f)) <= glm::radians(85.0f))
-		{
-			Orientation = newOrientation;
-		}
-
-		// Rotates the Orientation left and right
-		Orientation = glm::rotate(Orientation, glm::radians(-rotY), Up);
-
-		// Sets mouse cursor to the middle of the screen so that it doesn't end up roaming around
-		glfwSetCursorPos(window, (m_Width / 2), (m_Height / 2));
-
-	}
-	else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE)
-	{
-		// ToDo: This runs every frame, should have a shielding
-		//	     Preferably mouse and KB IO should be handled by a separate class
-		// 
-		// Unhides cursor since camera is not looking around anymore
-		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-		// Makes sure the next time the camera looks around it doesn't jump
-		firstClick = true;
-	}
-
 }
