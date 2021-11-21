76e739d428123088a494e1a3bacc18ba634af52d
torbjkri <torbjorn.kringeland88@gmail.com>
2021-11-21 01:51:50 +0100

    Loads scenes

--- OpenGL/Src/Whengine/SceneLoader.cpp
+++ OpenGL/Src/Whengine/SceneLoader.cpp
@@ -1,20 +1,47 @@
 #include "SceneLoader.h"
 
+#include <GL/Renderable/RenderableObject.h>
+#include <GL/Renderable/Cube.h>
+#include <GL/ResourceManager.h>
+
 #include <nlohmann/json.hpp>
 #include <string>
 #include <Whengine/Scene.h>
 #include <fstream>
 #include <iostream>
+#include <cstdlib>
 
 using json = nlohmann::json;
 
 namespace WE {
+	Cube LoadCube(json& obj)
+	{
+		try {
+			auto rm = ResourceManager::GetInstance();
+			Texture diffuse = rm->LoadTexture(obj["textures"]["diffuse"]["filepath"],
+				obj["textures"]["diffuse"]["name"], TextureType::DIFFUSE);
+			Texture spec = rm->LoadTexture(obj["textures"]["specular"]["filepath"],
+				obj["textures"]["specular"]["name"], TextureType::SPECULAR);
+			Shader shader = rm->LoadShader("Resources/Shaders/Lighting/BasicShader.glsl", "texture-cube");
+
+			return Cube(shader, diffuse, spec);
+		}
+		catch (json::exception& e) {
+			std::cout << e.what() << std::endl;
+			std::abort();
+		}
+	}
+
 	void LoadScene(std::string filename)
 	{
 		std::ifstream ifs(filename);
-		json obj = json::parse(ifs);
-		for (auto& elem : obj) {
-			std::cout << "\n" << elem[0]["type"];
+		json j = json::parse(ifs);
+		for (auto& [key,elem] : j.items()) {
+			if (key == "cube") {
+				for (auto& cubedata: elem)
+				Cube cube = LoadCube(cubedata);
+				
+			}
 		}
 	}
 }
