dc8a3817b4f59de6f18274df28a10df816210035
torbjkri <torbjorn.kringeland88@gmail.com>
2021-11-28 00:08:20 +0100

    Further component system

--- OpenGL/Src/ECS/RenderSystem.h
+++ OpenGL/Src/ECS/RenderSystem.h
@@ -1,8 +1,20 @@
 #pragma once
 
 #include "System.h"
+#include "Whengine/Scene.h"
 
 class RenderSystem : public System
 {
+private:
+	// The scene to which the system belongs
+	WE::Scene* m_ParentScene;
 
+public:
+	void Render() override
+	{
+		for (auto& entity : entities_) {
+			auto& renderable = m_ParentScene->GetComponent<RenderableCube>(entity);
+			renderable->cube_->Bind();
+		}
+	}
 };
