9184679f716e9c6782c87a67536595feb42809e8
torbjkri <torbjorn.kringeland88@gmail.com>
2021-12-09 23:56:04 +0100

    Hmm

--- OpenGL/Src/ECS/Components/Collidable.h
+++ OpenGL/Src/ECS/Components/Collidable.h
@@ -12,6 +12,17 @@ struct CollisionShape {
 	virtual ~CollisionShape() {};
 };
 
+struct CollisionSurface {
+	std::array<glm::vec3, 4> corners_ = 
+	{
+		glm::vec3( 1.0f,  1.0f, 1.0f),
+		glm::vec3( 1.0f, -1.0f, 1.0f),
+		glm::vec3(-1.0f,  1.0f, 1.0f),
+		glm::vec3(-1.0f, -1.0f, 1.0f)
+	};
+	glm::vec3 normal_ = glm::vec3(0.0f, 0.0f, 1.0f);
+};
+
 struct CollisionBox : public CollisionShape {
 	glm::vec3 size_ = glm::vec3(1.0f);
 };
