3f4697c5195959de2e958c5acb2993edbabbae2e
torbjkri <torbjorn.kringeland88@gmail.com>
2021-12-11 01:29:08 +0100

    Remove dead code

--- OpenGL/Src/ECS/Components/Transform.h
+++ OpenGL/Src/ECS/Components/Transform.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <glm/gtc/matrix_transform.hpp>
 #include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
 
 struct TransformState {
 	glm::vec3 position_;
@@ -12,4 +14,15 @@ struct Transform
 {
 	TransformState curr_state_;
 	TransformState prev_state_;
+	glm::mat4 CreateModelMatrix()
+	{
+		glm::mat4 R(1.0f);
+		R = glm::translate(R, curr_state_.position_);
+		R = glm::rotate(R, glm::radians(curr_state_.orientation_.x), glm::vec3(1.0f, 0.0f, 0.0f));
+		R = glm::rotate(R, glm::radians(curr_state_.orientation_.y), glm::vec3(0.0f, 1.0f, 0.0f));
+		R = glm::rotate(R, glm::radians(curr_state_.orientation_.z), glm::vec3(0.0f, 0.0f, 1.0f));
+		R = glm::scale(R, curr_state_.scale_);
+
+		return R;
+	}
 };
--- OpenGL/Src/ECS/Systems/CubeRenderSystem.h
+++ OpenGL/Src/ECS/Systems/CubeRenderSystem.h
@@ -16,21 +16,6 @@
 
 class CubeRenderSystem : public RenderSystem
 {
-private:
-
-	glm::mat4 CreateModelMatrix(TransformState& transform)
-	{
-		glm::mat4 R(1.0f);
-		R = glm::translate(R, transform.position_);
-		R = glm::rotate(R, glm::radians(transform.orientation_.x), glm::vec3(1.0f, 0.0f, 0.0f));
-		R = glm::rotate(R, glm::radians(transform.orientation_.y), glm::vec3(0.0f, 1.0f, 0.0f));
-		R = glm::rotate(R, glm::radians(transform.orientation_.z), glm::vec3(0.0f, 0.0f, 1.0f));
-		R = glm::scale(R, transform.scale_);
-
-		return R;
-
-	}
-
 public:
 	CubeRenderSystem(std::shared_ptr<WE::Scene> scene)
 		: RenderSystem(scene)
@@ -54,8 +39,8 @@ public:
 			renderable.cube_->Bind();
 			renderable.shader_->SetUniform4fv("u_Color", 1, renderable.color_);
 
-			glm::mat4 R = CreateModelMatrix(transform.curr_state_);
-			R = modelView * R;
+			glm::mat4 R = transform.CreateModelMatrix();
+			R = modelView * R ;
 
 			renderable.shader_->SetUniformMatrix4fv("u_ProjectionViewModel", 1, R);
 
